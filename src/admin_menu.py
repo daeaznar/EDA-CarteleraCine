c8c012ae496fb077aaaddcb15eadc6bbd3588e41
Daeaznar <dae.aznar@gmail.com>
2021-11-28 21:47:09 -0600

    Added function to add new admin user accounts

--- src/admin_menu.py
+++ src/admin_menu.py
@@ -1,3 +1,7 @@
+import sqlite3
+import mask_password
+from classes import User
+
 
 def admin_menu():
     print()
@@ -11,6 +15,7 @@ def admin_menu():
             5. Modify Movie
             6. View Movie
             7. Full cinema schedule
+            8. Create admin account
             0. Log Out
             """)
         try:
@@ -32,6 +37,8 @@ def admin_menu():
                 pass
             elif opt == 7:
                 pass
+            elif opt == 8:
+                register()
             elif opt == 0:
                 while True:
                     confirm = input("Are you sure you want to log out? (y/n)>>")
@@ -48,3 +55,52 @@ def admin_menu():
                     break
             else:
                 print("Invalid Option\n")
+
+
+# Add new user account to DB
+def register():
+    conn = sqlite3.connect('cinema.db')
+    cursor = conn.cursor()
+    print()
+    print("*Register*")
+
+    while True:
+        print("Enter your information (press 0 to cancel)\n")
+        user_name = input("User Name: ")
+        if user_name == '0':
+            break
+
+        cursor.execute("SELECT user_name FROM user WHERE user_name = :user_name", {'user_name': user_name})
+        check_user = cursor.fetchall()
+
+        if len(check_user) != 0:
+            print("user_name is already taken\n")
+        else:
+            print()
+            while True:
+                temp_password = mask_password.hide_pass()  # Prevent password from showing input
+                password = mask_password.hide_pass(prompt='Confirm Password: ')
+                if password == temp_password:
+                    break
+                else:
+                    print("Error. Password doesn't match")
+            while True:
+                confirm = input("Is your information correct? (y/n)>>")
+                if confirm == 'y':
+                    user = User(user_name, password)
+
+                    with conn:
+                        cursor.execute("INSERT INTO user (user_name, password)"
+                                       "VALUES(:user_name, :password)",
+                                       {'user_name': user.user_name,
+                                        'password': user.password})
+                    break
+                elif confirm == 'n':
+                    print()
+                    break
+                else:
+                    print("Invalid Option\n")
+            if confirm == 'y':
+                conn.close()
+                break
+        break
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import mask_password
 import admin_menu
 import client_menu
 from classes import User
+
 # Define connection and cursor2
 conn = sqlite3.connect('cinema.db')
 cursor = conn.cursor()
@@ -75,66 +76,6 @@ def login():
             admin_menu.admin_menu()
             break
 
-
-# def register():
-#     print()
-#     print("*Register*")
-#     while True:
-#         print("Enter your information (press 0 to cancel)\n")
-#         first_name = input("First Name: ")
-#         if first_name == '0':
-#             break
-#         last_name = input("Last Name: ")
-#         user_name = input("user_name: ")
-#
-#         cursor.execute("SELECT user_name FROM user WHERE user_name = :user_name", {'user_name': user_name})
-#         check_mail = cursor.fetchall()
-#
-#         if len(check_mail) != 0:
-#             print("user_name is already taken\n")
-#         else:
-#             print()
-#             while True:
-#                 temp_password = mask_password.hide_pass()  # Prevent password from showing input
-#                 password = mask_password.hide_pass(prompt='Confirm Password: ')
-#                 if password == temp_password:
-#                     break
-#                 else:
-#                     print("Error. Password doesn't match")
-#             while True:
-#                 confirm = input("Is your information correct? (y/n)>>")
-#                 if confirm == 'y':
-#                     user = User(first_name, last_name, user_name, password)
-#
-#                     with conn:
-#                         cursor.execute("INSERT INTO user (first_name, last_name, user_name, password)"
-#                                        "VALUES(:first_name, :last_name, :user_name, :password)",
-#                                        {'first_name': user.first_name, 'last_name': user.last_name, 'user_name': user.user_name,
-#                                         'password': user.password})
-#
-#                         cursor.execute("SELECT user_id FROM user WHERE user_name = :user_name", {'user_name': user.user_name})
-#                         user_id = cursor.fetchone()[0]
-#
-#                         cursor.execute("INSERT INTO account (currency, user_id)"
-#                                        "VALUES(:currency, :user_id)",
-#                                        {'currency': 'MXN', 'user_id': user_id})
-#
-#                         cursor.execute("SELECT account_id FROM account WHERE user_id = :user_id", {'user_id': user_id})
-#                         account_id = cursor.fetchone()[0]
-#
-#                         account = Account(account_id, 0, 0, 'MXN', user_id)
-#
-#                     home(user, account)
-#                     break
-#                 elif confirm == 'n':
-#                     print()
-#                     break
-#                 else:
-#                     print("Invalid Option\n")
-#             if confirm == 'y':
-#                 break
-#         break
-#
 
 if __name__ == '__main__':
     main()
